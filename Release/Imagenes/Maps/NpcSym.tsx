faab40222effcc227b46ff52bf8979d70b8d3358
poseidon9 <poseidonthenine@gmail.comm>
2019-01-26 13:56:51 +0800

    Actualizado carpeta Imagenes de la carpeta Release

---
+++ Release/Imagenes/Maps/NpcSym.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="NpcSym" tilewidth="16" tileheight="16" tilecount="3200" columns="80">
+ <image source="../Tiles/BackGround1.png" width="1280" height="640"/>
+ <tile id="437">
+  <animation>
+   <frame tileid="438" duration="100"/>
+   <frame tileid="439" duration="100"/>
+   <frame tileid="440" duration="100"/>
+   <frame tileid="367" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
