c82e2130b59a75b903344635c0d6f8e891016f58
Zkauba <jisuscrust4108@gmail.com>
2019-05-11 21:13:34 -0600

    Actualizacion de tiles

---
+++ Release/Imagenes/Tiles/Arbol Jungla.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Arbol Jungla" tilewidth="16" tileheight="16" tilecount="169" columns="13">
+ <image source="Users/Ruben/Desktop/Ultimate Mayan/ASSETS ALL/Assets Backgrounds/tree.png" width="213" height="223"/>
+</tileset>
